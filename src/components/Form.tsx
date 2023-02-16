e59b2bb812c79c78686e9d813e8beaf0aa1b545b
Renan Sigolo <renan.sigolo@gmail.com>
2023-02-16 12:44:00 +1100

    feat: add form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,28 +1,28 @@
-import { useState, type FormEvent } from "react";
+import { createSignal, Show } from "solid-js";
 
 type FormProps = {
   setSuccess: (success: boolean) => void;
 };
-export function Form({ setSuccess }: FormProps) {
-  const [loading, setLoading] = useState(false);
 
-  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+export function Form({ setSuccess }: FormProps) {
+  const [loading, setLoading] = createSignal(false);
+  const [email, setEmail] = createSignal("");
+
+  async function handleSubmit(e: SubmitEvent) {
     e.preventDefault();
     setLoading(true);
 
     // Read the form data
-    const form = e.target as HTMLFormElement;
-    const formData = new FormData(form);
-    const formJson = Object.fromEntries(formData.entries());
 
-    fetch(`${import.meta.env.PUBLIC_API_URL}/saveRegistration`, {
-      method: "post",
+    const formData = { email: email() };
+
+    await fetch(`${import.meta.env.PUBLIC_API_URL}/saveRegistration`, {
+      method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(formJson),
+      body: JSON.stringify(formData),
     })
       .then((res) => {
         if (res.ok) {
-          form.reset();
           setSuccess(true);
         } else {
           throw new Error("Something went wrong");
@@ -37,32 +37,33 @@ export function Form({ setSuccess }: FormProps) {
   }
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="mt-12 sm:mx-auto sm:flex sm:max-w-lg"
-    >
-      <div className="min-w-0 flex-1">
-        <label htmlFor="email" className="sr-only">
+    <form onSubmit={handleSubmit} class="mt-12 sm:mx-auto sm:flex sm:max-w-lg">
+      <div class="min-w-0 flex-1">
+        <label for="email" class="sr-only">
           Email address
         </label>
         <input
           required
-          disabled={loading}
-          className="block w-full rounded-md border border-transparent px-5 py-3 text-base text-gray-900 placeholder-gray-500 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600"
+          disabled={loading()}
+          class="block w-full rounded-md border border-transparent px-5 py-3 text-base text-gray-900 placeholder-gray-500 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600"
           type="email"
           id="email"
           name="email"
           placeholder="Enter your email"
+          value={email()}
+          onInput={(e) => setEmail(e.currentTarget.value)}
         />
       </div>
 
-      <div className="mt-4 sm:mt-0 sm:ml-3">
+      <div class="mt-4 sm:mt-0 sm:ml-3">
         <button
           type="submit"
-          disabled={loading}
-          className="block w-full rounded-md border border-transparent bg-indigo-500 px-5 py-3 text-base font-medium text-white shadow hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600 sm:px-10"
+          disabled={loading()}
+          class="block w-full rounded-md border border-transparent bg-indigo-500 px-5 py-3 text-base font-medium text-white shadow hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600 sm:px-10"
         >
-          {loading ? <span aria-busy={loading}>Sending...</span> : "Notify me"}
+          <Show when={loading()} fallback="Notify Me">
+            <span aria-busy={loading()}>Sending...</span>
+          </Show>
         </button>
       </div>
     </form>
--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,39 +1,39 @@
-import { useState } from "react";
-import { Form } from "./Form";
+import { Form } from "@/components/Form";
+import { createSignal } from "solid-js";
 
-export default function RegisterForm() {
-  const [success, setSuccess] = useState(false);
+export function Register() {
+  const [success, setSuccess] = createSignal(false);
 
   return (
-    <div className="bg-white py-16 sm:py-24">
-      <div className="relative sm:py-16">
-        <div className="mx-auto max-w-md px-6 sm:max-w-3xl lg:max-w-7xl lg:px-8">
-          <div className="relative overflow-hidden rounded-2xl bg-indigo-600 px-6 py-10 shadow-xl sm:px-12 sm:py-20">
+    <div class="bg-white py-16 sm:py-24">
+      <div class="relative sm:py-16">
+        <div class="mx-auto max-w-md px-6 sm:max-w-3xl lg:max-w-7xl lg:px-8">
+          <div class="relative overflow-hidden rounded-2xl bg-indigo-600 px-6 py-10 shadow-xl sm:px-12 sm:py-20">
             <div
               aria-hidden="true"
-              className="absolute inset-0 -mt-72 sm:-mt-32 md:mt-0"
+              class="absolute inset-0 -mt-72 sm:-mt-32 md:mt-0"
             >
               <svg
-                className="absolute inset-0 h-full w-full"
+                class="absolute inset-0 h-full w-full"
                 preserveAspectRatio="xMidYMid slice"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 1463 360"
               >
                 <path
-                  className="text-indigo-500 text-opacity-40"
+                  class="text-indigo-500 text-opacity-40"
                   fill="currentColor"
                   d="M-82.673 72l1761.849 472.086-134.327 501.315-1761.85-472.086z"
                 />
                 <path
-                  className="text-indigo-700 text-opacity-40"
+                  class="text-indigo-700 text-opacity-40"
                   fill="currentColor"
                   d="M-217.088 544.086L1544.761 72l134.327 501.316-1761.849 472.086z"
                 />
               </svg>
             </div>
-            <div className="relative">
-              {success ? (
+            <div class="relative">
+              {success() ? (
                 <Heading
                   title="Thank you for your registration."
                   description="We'll let you know know when we're ready to go!"
@@ -61,11 +61,11 @@ type HeadingProps = {
 };
 function Heading({ title, description }: HeadingProps) {
   return (
-    <div className="sm:text-center">
-      <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+    <div class="sm:text-center">
+      <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">
         {title}
       </h2>
-      <p className="mx-auto mt-6 max-w-2xl text-lg text-indigo-200">
+      <p class="mx-auto mt-6 max-w-2xl text-lg text-indigo-200">
         {description}
       </p>
     </div>
